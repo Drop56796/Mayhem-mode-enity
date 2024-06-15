939cf94b309b60f1570819b2283aa0b08a034e73
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-06-15 23:13:28 +0800

    Create End check.lua

---
+++ End check.lua
@@ -0,0 +1,9 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Mayhem-mode-enity/main/check1.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Mayhem-mode-enity/main/check2.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Mayhem-mode-enity/main/check3.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Mayhem-mode-enity/main/check4.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hoangnguyen72628/MayhemMode/main/ObfuscatedEntities/Fog-obfuscated.lua"))()
