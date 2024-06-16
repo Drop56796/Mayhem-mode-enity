3fc8e231906e51fca85f5e436cc708cc82248477
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-06-16 22:41:19 +0800

    Update End check.lua

--- End check.lua
+++ End check.lua
@@ -1,9 +1,63 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Mayhem-mode-enity/main/check1.lua"))()
+--生成时生成雾
+local lighting = game:GetService("Lighting")
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Mayhem-mode-enity/main/check2.lua"))()
+lighting.FogColor = Color3.new(0, 0, 0)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Mayhem-mode-enity/main/check3.lua"))()
+lighting.FogStart = 0
+lighting.FogEnd = 150
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Mayhem-mode-enity/main/check4.lua"))()
+--进入图书馆变成白雾
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
+if game.ReplicatedStorage.GameData.LatestRoom.Value == 50 then
+local lighting = game:GetService("Lighting")
 
+lighting.FogColor = Color3.new(1, 1, 1)
+
+lighting.FogStart = 5
+lighting.FogEnd = 50
+end
+end)
+
+--出图书馆变成黑雾
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
+if game.ReplicatedStorage.GameData.LatestRoom.Value == 51 then
+local lighting = game:GetService("Lighting")
+
+lighting.FogColor = Color3.new(0, 0, 0)
+
+lighting.FogStart = 0
+lighting.FogEnd = 150
+end
+end)
+
+--达到温室变成白雾
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
+if game.ReplicatedStorage.GameData.LatestRoom.Value == 90 then
+local lighting = game:GetService("Lighting")
+
+lighting.FogColor = Color3.new(1, 1, 1)
+
+lighting.FogStart = 5
+lighting.FogEnd = 50
+end
+end)
+
+--达到电力室变回
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
+if game.ReplicatedStorage.GameData.LatestRoom.Value == 100 then
+local lighting = game:GetService("Lighting")
+
+lighting.FogColor = Color3.new(0, 0, 0)
+
+lighting.FogStart = 0
+lighting.FogEnd = 150
+end
+end)
+
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
+if game.ReplicatedStorage.GameData.LatestRoom.Value == 50 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hoangnguyen72628/MayhemMode/main/ObfuscatedEntities/Fog-obfuscated.lua"))()
+
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
+if game.ReplicatedStorage.GameData.LatestRoom.Value == 100 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Hoangnguyen72628/MayhemMode/main/ObfuscatedEntities/Fog-obfuscated.lua"))()
