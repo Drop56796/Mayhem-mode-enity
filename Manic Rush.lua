845fc25560ad160c64b701123bdf9e1503c42ee3
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-08-09 12:25:52 +0800

    Update Manic Rush.lua

--- Manic Rush.lua
+++ Manic Rush.lua
@@ -1,109 +1,77 @@
-local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
+--===== Define spawner ======---
+
+local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V1/Source.lua"))()
 
 ---====== Create entity ======---
 
-local entity = spawner.Create({
-	Entity = {
-		Name = "Mangle",
-		Asset = "https://github.com/Jahani-john/mayhem-mode/blob/main/mayhemmode-main/NMRush.rbxm?raw=true",
-		HeightOffset = 2
-	},
-	Lights = {
-		Flicker = {
-			Enabled = true,
-			Duration = 0
-		},
-		Shatter = true,
-		Repair = false
-	},
-	CameraShake = {
-		Enabled = true,
-		Range = 100,
-		Values = {1.5, 20, 0.1, 1} -- Magnitude, Roughness, FadeIn, FadeOut
-	},
-	Movement = {
-		Speed = 500,
-		Delay = 0,
-		Reversed = false
-	},
-	Rebounding = {
-		Enabled = true,
-		Type = "Ambush", -- "Blitz"
-		Min = 1,
-		Max = 6,
-		Delay = 0.75
-	},
-	Damage = {
-		Enabled = true,
-		Range = 40,
-		Amount = 125
-	},
-	Crucifixion = {
-		Enabled = true,
-		Range = 40,
-		Resist = false,
-		Break = true
-	},
-	Death = {
-		Type = "Guiding", -- "Curious"
-		Hints = {"You died to Manic Rush", "He is faster than ambush..", "You must Avoid him..", "Good luck..."},
-		Cause = "Manic Rush"
-	}
+local entity = Spawner.createEntity({
+    CustomName = "Manci Rush",
+    Model = "https://github.com/Jahani-john/mayhem-mode/blob/main/mayhemmode-main/NMRush.rbxm?raw=true", -- Your entity's model url here ("rbxassetid://1234567890" or GitHub raw url)
+    Speed = 350,
+    MoveDelay = 0.5,
+    HeightOffset = 0,
+    CanKill = true,
+    KillRange = 100,
+    SpawnInFront = false,
+    ShatterLights = true,
+    FlickerLights = {
+        Enabled = true,
+        Duration = 0.5
+    },
+    Cycles = {
+        Min = 1,
+        Max = 3,
+        Delay = 0.25
+    },
+    CamShake = {
+        Enabled = true,
+        Values = {1.5, 20, 0.1, 1},
+        Range = 100
+    },
+    ResistCrucifix = false,
+    BreakCrucifix = true,
+    DeathMessage = {"You died him", "Goodbye", "...", "...", "..."},
+    IsCuriousLight = false
 })
 
----====== Debug entity ======---
+---====== Debug ======---
 
-entity:SetCallback("OnSpawned", function()
+entity.Debug.OnEntitySpawned = function()
     print("Entity has spawned")
-end)
+end
 
-entity:SetCallback("OnStartMoving", function()
-    print("Entity has started moving")
-end)
-
-entity:SetCallback("OnReachNode", function(node)
-	print("Entity has reached node:", node)
-end)
-
-entity:SetCallback("OnEnterRoom", function(room, firstTime)
-    if firstTime == true then
-        print("Entity has entered room: ".. room.Name.. " for the first time")
-    else
-        print("Entity has entered room: ".. room.Name.. " again")
-    end
-end)
-
-entity:SetCallback("OnLookAt", function(lineOfSight)
-	if lineOfSight == true then
-		print("Player is looking at entity")
-	else
-		print("Player view is obstructed by something")
-	end
-end)
-
-entity:SetCallback("OnRebounding", function(startOfRebound)
-    if startOfRebound == true then
-        print("Entity has started rebounding")
-	else
-        print("Entity has finished rebounding")
-	end
-end)
-
-entity:SetCallback("OnDespawning", function()
-    wait(1)
-    print("Entity despawning")
-end)
-
-entity:SetCallback("OnDespawned", function()
+entity.Debug.OnEntityDespawned = function()
     print("Entity has despawned")
-end)
+end
 
-entity:SetCallback("OnDamagePlayer", function(newHealth)
-	if newHealth == 0 then
-		print("Entity has killed the player")
-	else
-		print("Entity has damaged the player")
-	end
-end)
+entity.Debug.OnEntityStartMoving = function()
+    print("Entity started moving")
+end
 
-entity:Run()
+entity.Debug.OnEntityFinishedRebound = function()
+    print("Entity finished rebound")
+end
+
+entity.Debug.OnEntityEnteredRoom = function(room)
+    print("Entity entered room:", room)
+end
+
+entity.Debug.OnLookAtEntity = function()
+    print("Player looking at entity")
+end
+
+entity.Debug.OnDeath = function()
+    print("Player has died")
+end
+
+--[[
+    NOTE: By overwriting 'OnUseCrucifix', the default crucifixion will be ignored and this function will be called instead
+
+    entity.Debug.OnUseCrucifix = function()
+        print("Custom crucifixion script here")
+    end
+]]--
+
+---====== Run entity ======---
+
+Spawner.runEntity(entity)
