fc0c7bfdd3d739737333590df3dcf320b9b6e086
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-10-01 15:02:57 +0800

    Update Hard Obsession.lua

--- Hard Obsession.lua
+++ Hard Obsession.lua
@@ -2,80 +2,125 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Jahani-john/mayhem-mo
 game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
 
 
----====== Define spawner ======---
+---====== Load spawner ======---
 
-local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V1/Source.lua"))()
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
 
 ---====== Create entity ======---
 
-local entity = Spawner.createEntity({
-    CustomName = "Obsession",
-    Model = "https://github.com/Jahani-john/mayhem-mode/blob/main/mayhemmode-main/Greed.Obssesion.rbxm?raw=true", -- Your entity's model url here ("rbxassetid://1234567890" or GitHub raw url)
-    Speed = 125,
-    MoveDelay = 1,
-    HeightOffset = 0,
-    CanKill = true,
-    KillRange = 50,
-    SpawnInFront = false,
-    ShatterLights = true,
-    FlickerLights = {
-        Enabled = true,
-        Duration = 1
-    },
-    Cycles = {
-        Min = 1,
-        Max = 1,
-        Delay = 0
-    },
-    CamShake = {
-        Enabled = true,
-        Values = {1.5, 20, 0.1, 1},
-        Range = 100
-    },
-    ResistCrucifix = false,
-    BreakCrucifix = true,
-    DeathMessage = {"You died with Obsession", "U know he is hard", "Good luck", "..", ".."},
-    IsCuriousLight = false
+local entity = spawner.Create({
+	Entity = {
+		Name = "Obsession",
+		Asset = "https://github.com/Jahani-john/mayhem-mode/blob/main/mayhemmode-main/Greed.Obssesion.rbxm?raw=true",
+		HeightOffset = 0
+	},
+	Lights = {
+		Flicker = {
+			Enabled = true,
+			Duration = 1
+		},
+		Shatter = true,
+		Repair = false
+	},
+	Earthquake = {
+		Enabled = true
+	},
+	CameraShake = {
+		Enabled = true,
+		Range = 100,
+		Values = {1.5, 20, 0.1, 1} -- Magnitude, Roughness, FadeIn, FadeOut
+	},
+	Movement = {
+		Speed = 100,
+		Delay = 2,
+		Reversed = false
+	},
+	Rebounding = {
+		Enabled = false,
+		Type = "Ambush", -- "Blitz"
+		Min = 1,
+		Max = 1,
+		Delay = 0
+	},
+	Damage = {
+		Enabled = true,
+		Range = 100,
+		Amount = 125
+	},
+	Crucifixion = {
+		Enabled = true,
+		Range = 40,
+		Resist = false,
+		Break = true
+	},
+	Death = {
+		Type = "Curious", -- "Curious"
+		Hints = {"Oh hi", "I'm light", "u died because Obsession attack with you", "bye"},
+		Cause = "Obsession"
+	}
 })
 
----====== Debug ======---
+---====== Debug entity ======---
 
-entity.Debug.OnEntitySpawned = function()
+entity:SetCallback("OnSpawned", function()
     print("Entity has spawned")
-end
+end)
 
-entity.Debug.OnEntityDespawned = function()
+entity:SetCallback("OnStartMoving", function()
+    print("Entity has started moving")
+end)
+
+entity:SetCallback("OnEnterRoom", function(room, firstTime)
+    if firstTime == true then
+        print("Entity has entered room: ".. room.Name.. " for the first time")
+    else
+        print("Entity has entered room: ".. room.Name.. " again")
+    end
+end)
+
+entity:SetCallback("OnLookAt", function(lineOfSight)
+	if lineOfSight == true then
+		print("Player is looking at entity")
+	else
+		print("Player view is obstructed by something")
+	end
+end)
+
+entity:SetCallback("OnRebounding", function(startOfRebound)
+    if startOfRebound == true then
+        print("Entity has started rebounding")
+	else
+        print("Entity has finished rebounding")
+	end
+end)
+
+entity:SetCallback("OnDespawning", function()
+    print("Entity is despawning")
+end)
+
+entity:SetCallback("OnDespawned", function()
     print("Entity has despawned")
-end
+end)
 
-entity.Debug.OnEntityStartMoving = function()
-    print("Entity started moving")
-end
-
-entity.Debug.OnEntityFinishedRebound = function()
-    print("Entity finished rebound")
-end
-
-entity.Debug.OnEntityEnteredRoom = function(room)
-    print("Entity entered room:", room)
-end
-
-entity.Debug.OnLookAtEntity = function()
-    print("Player looking at entity")
-end
-
-entity.Debug.OnDeath = function()
-    print("Player has died")
-end
+entity:SetCallback("OnDamagePlayer", function(newHealth)
+	if newHealth == 0 then
+		print("Entity has killed the player")
+	else
+		print("Entity has damaged the player")
+	end
+end)
 
 --[[
-    NOTE: By overwriting 'OnUseCrucifix', the default crucifixion will be ignored and this function will be called instead
 
-    entity.Debug.OnUseCrucifix = function()
-        print("Custom crucifixion script here")
-    end
+DEVELOPER NOTE:
+By overwriting 'CrucifixionOverwrite' the default crucifixion callback will be replaced with your custom callback.
+
+entity:SetCallback("CrucifixionOverwrite", function()
+    print("Custom crucifixion callback")
+end)
+
 ]]--
 
 ---====== Run entity ======---
 
-Spawner.runEntity(entity)
+entity:Run()
